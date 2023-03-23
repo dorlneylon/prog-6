4127228ea31d9d51c4aa7104096a478e3ba92d2c
nylon <dorlneylon@gmail.com>
2023-03-23 18:54:25 +0300

    cleanup

--- src/auxiliary/Writable.java
+++
@@ -1,6 +0,0 @@
-package src.auxiliary;
-
-public interface Writable {
-    String takeEntry();
-    String takeEntryName();
-}
--- src/utils/xml/XmlAction.java
+++
@@ -1,9 +0,0 @@
-package itmo.kxrxh.lab5.utils.xml;
-
-public abstract class XmlAction {
-    protected Xml xml;
-
-    public XmlAction(Xml xml) {
-        this.xml = xml;
-    }
-}
